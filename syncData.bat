c40f8f54a2812f3830c661a8a750aba7544cd7ae
hrk76 <72525208+hrk76@users.noreply.github.com>
2020-10-26 00:13:04 -0700

    Update syncData.bat

--- syncData.bat
+++ syncData.bat
@@ -6,6 +6,7 @@ ren dds%date:~-10,2%%date:~-7,2%.xls dds.xls
 :: Renames inventory_export_MMDD.csv to inventory_export.csv
 ren inventory_export_%date:~-10,2%%date:~-7,2%.csv inventory_export.csv
 
+:: Change first directory to your python.exe
 "C:\Program Files\Python37\python.exe" "C:\SyncINV\syncData.py"
 
 PAUSE 
